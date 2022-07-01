7180fdaf17c02c9ff431de3ffde12d86cab52e9d
Jarrod Bogard <jarrodbogard@gmail.com>
2022-06-30 19:03:11 -0500

    stuff

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-require("dotenv").config()
-// console.log(process.env)
-
 const express = require("express")
 const app = express()
 const connection = require("./Sql/connection")
-// console.log(connection)
 
 app.use(express.json())
 
