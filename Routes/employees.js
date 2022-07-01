7331617937548d10f0e7fa6230e2695e3387df37
Jarrod Bogard <jarrodbogard@gmail.com>
2022-06-30 21:06:29 -0500

    working on controllers

--- Routes/employees.js
+++ Routes/employees.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const { getEmployees, getEmployeesById, getEmployeesByFirstName } = require('../controllers/employees')
-const router = express.Router()
-  
-router.get('/', getEmployees);
-router.get('/:id', getEmployeesById);
-router.get('firstname/:first_name', getEmployeesByFirstName);
+const express = require("express");
+const {
+  getEmployees,
+  getEmployeesById,
+  getEmployeesByFirstName,
+} = require("../controllers/employees");
+const router = express.Router();
 
-module.exports = router
+router.get("/", getEmployees);
+router.get("/:id", getEmployeesById);
+router.get("/:first_name", getEmployeesByFirstName);
 
+module.exports = router;
--- controllers/employees.js
+++ controllers/employees.js
@@ -1,23 +1,48 @@
-// const employees = require('../index')
 const mysql = require("mysql");
 const pool = require("../sql/connection");
 
-
 const getEmployees = (req, res) => {
-    
+  pool.query("SELECT * FROM employees LIMIT 10", (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
 };
 
 const getEmployeesById = (req, res) => {
-    
+  const { id } = req.params;
+
+  let sql = "SELECT * FROM ?? Where ?? = ?";
+  const replacements = ["employees", "id", id];
+  sql = mysql.format(sql, replacements);
+  pool.query(sql, (err, rows) => {
+    if (err) {
+      console.log({ message: "Error occurred: " + err });
+      return res.status(500).send("An unexpected error occurred");
+    }
+    res.json(rows);
+  });
 };
 
 const getEmployeesByFirstName = (req, res) => {
-    
+    console.log(req.params)
+    const { first_name } = req.params;
+    console.log({first_name});
+
+  let sql = "SELECT ?? FROM ?? WHERE ?? ";
+  const replacements = ["first_name", "employees", "first_name"];
+  sql = mysql.format(sql, replacements);
+
+  pool.query(sql, (err, rows) => {
+    if (err) {
+      console.log({ message: "Error occurred: " + err });
+      return res.status(500).send("An unexpected error occurred");
+    }
+    res.json(rows);
+  });
 };
 
-
-module.exports = { 
-    getEmployees, 
-    getEmployeesById, 
-    getEmployeesByFirstName 
-}
+module.exports = {
+  getEmployees,
+  getEmployeesById,
+  getEmployeesByFirstName,
+};
