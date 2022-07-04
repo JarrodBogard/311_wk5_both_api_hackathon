4ab5e8f1d23af42267a6f4387e76c7d796d8749e
Jarrod Bogard <jarrodbogard@gmail.com>
2022-07-04 12:20:15 -0500

    latest

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express")
 const app = express()
 const employeesRouter = require('./Routes/employees')
-const connection = require("./Sql/connection")
-
 
 app.use(express.json())
 
 app.use('/employees', employeesRouter)
-
 
 app.get("/", (req, res) => {
     res.send("welcome to our page")
