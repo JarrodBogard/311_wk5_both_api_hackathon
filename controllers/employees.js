8e962c78092d33c4900b1ec58a7a64d19286fadb
MarisolM09 <mmendez9980@gmail.com>
2022-07-05 21:35:27 -0500

    Added Address Controllers and Routes

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql");
 const pool = require("../sql/connection");
+const { handleSQLError } = require('../sql/error')
+
 
 const getEmployees = (req, res) => {
   pool.query("SELECT * FROM employees LIMIT 10", (err, rows) => {
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 const employeesRouter = require('./routes/employees')
 const contactRouter = require('./routes/contact')
+const addressRouter = require('./routes/address')
 
 app.use(express.json())
 
 app.use('/employees', employeesRouter)
 app.use('/contact', contactRouter)
+app.use('/address', addressRouter)
 
 app.get("/", (req, res) => {
     res.send("welcome to our page")
